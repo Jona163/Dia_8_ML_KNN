51300e68ed68eeed4c100054d9cdf0d182c88f83
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-09 09:40:57 -0600

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -1,1 +1,14 @@
+import numpy as np
+from collections import Counter
 
+def euclidean_distance(x1, x2):
+    distance = np.sqrt(np.sum((x1-x2)**2))
+    return distance
+
+class KNN:
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
