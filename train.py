9ab0129ce0558454ba36ebba53b71dce127e82c3
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-09 09:46:09 -0600

    Update train.py

--- train.py
+++ train.py
@@ -4,3 +4,5 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from KNN import KNN
+
+cmap = ListedColormap(['#FF0000','#00FF00','#0000FF'])
