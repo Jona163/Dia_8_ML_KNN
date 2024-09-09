9443fdc051f6d6261123f420c05f12cf6247f03d
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-09 09:41:11 -0600

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -12,3 +12,19 @@ class KNN:
     def fit(self, X, y):
         self.X_train = X
         self.y_train = y
+
+ def predict(self, X):
+        predictions = [self._predict(x) for x in X]
+        return predictions
+
+    def _predict(self, x):
+        # compute the distance
+        distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+    
+        # get the closest k
+        k_indices = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+
+        # majority voye
+        most_common = Counter(k_nearest_labels).most_common()
+        return most_common[0][0]
