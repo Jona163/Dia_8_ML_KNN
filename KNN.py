b0d509d9f8c9858771ea55337fc61be314a486eb
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-09 09:43:35 -0600

    Final KNN.py

--- KNN.py
+++ KNN.py
@@ -1,10 +1,13 @@
+#Importacion de librerias 
 import numpy as np
 from collections import Counter
 
+#Urtilizacion de distancia euclidiana 
 def euclidean_distance(x1, x2):
     distance = np.sqrt(np.sum((x1-x2)**2))
     return distance
 
+#Clase de algortitmo KNN 
 class KNN:
     def __init__(self, k=3):
         self.k = k
@@ -12,19 +15,20 @@ class KNN:
     def fit(self, X, y):
         self.X_train = X
         self.y_train = y
-
- def predict(self, X):
+        
+#Definicion de la prediccion a realizar
+    def predict(self, X):
         predictions = [self._predict(x) for x in X]
         return predictions
 
     def _predict(self, x):
-        # compute the distance
+        # calcular la distancia 
         distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
     
-        # get the closest k
+        # obteniedo el closet
         k_indices = np.argsort(distances)[:self.k]
         k_nearest_labels = [self.y_train[i] for i in k_indices]
 
-        # majority voye
+        # mayoria de voye
         most_common = Counter(k_nearest_labels).most_common()
         return most_common[0][0]
