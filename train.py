42ceb4a3aafadfe10ad8a3e808d9570d5c2e4d68
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-09 09:52:48 -0600

    Update train.py

--- train.py
+++ train.py
@@ -13,17 +13,20 @@ cmap = ListedColormap(['#FF0000','#00FF00','#0000FF'])
 iris = datasets.load_iris()
 X, y = iris.data, iris.target
 
+#Definiendo los parametros del entrenamiento 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
+#Utilizacion de Matplotlib para graficar los parametros que nos devolvera el entramiento de Ml.
 plt.figure()
 plt.scatter(X[:,2],X[:,3], c=y, cmap=cmap, edgecolor='k', s=20)
 plt.show()
 
-
+#Se busca predecir con el algortimo KNN
 clf = KNN(k=5)
 clf.fit(X_train, y_train)
 predictions = clf.predict(X_test)
 
+#Se imprimen las predicciones del algortimo de KNN en consola
 print(predictions)
 
 acc = np.sum(predictions == y_test) / len(y_test)
