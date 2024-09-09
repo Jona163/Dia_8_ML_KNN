2fe7417d12f9ebcc753c827dfa37589c7a6d6a24
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-09 09:47:03 -0600

    Update train.py

--- train.py
+++ train.py
@@ -11,3 +11,6 @@ iris = datasets.load_iris()
 X, y = iris.data, iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+plt.figure()
+plt.scatter(X[:
