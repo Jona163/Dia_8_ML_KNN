e18243a8e363945e3bea74cb345872ae11c64f1e
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-09 09:46:43 -0600

    Update train.py

--- train.py
+++ train.py
@@ -9,3 +9,5 @@ cmap = ListedColormap(['#FF0000','#00FF00','#0000FF'])
 
 iris = datasets.load_iris()
 X, y = iris.data, iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
