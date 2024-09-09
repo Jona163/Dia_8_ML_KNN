ad9af21390b5d1bf59bec716a7f7ba1b87d5bbbd
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-09 09:45:39 -0600

    Update train.py

--- train.py
+++ train.py
@@ -1,1 +1,6 @@
-
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from KNN import KNN
